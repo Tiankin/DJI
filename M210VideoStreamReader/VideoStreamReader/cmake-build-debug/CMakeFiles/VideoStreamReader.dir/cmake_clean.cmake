08b29d12c16cfb0c3c17bdb10cb463e54f5a0479
Tiankin <53459017+Tiankin@users.noreply.github.com>
2021-02-10 04:22:21 +0900

    Add files via upload

---
+++ M210VideoStreamReader/VideoStreamReader/cmake-build-debug/CMakeFiles/VideoStreamReader.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/VideoStreamReader.dir/common/dji_linux_environment.cpp.o"
+  "CMakeFiles/VideoStreamReader.dir/common/dji_linux_helpers.cpp.o"
+  "CMakeFiles/VideoStreamReader.dir/main.cpp.o"
+  "CMakeFiles/VideoStreamReader.dir/SocketFileServer.cpp.o"
+  "CMakeFiles/VideoStreamReader.dir/TCPServer.cpp.o"
+  "VideoStreamReader.pdb"
+  "VideoStreamReader"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/VideoStreamReader.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
