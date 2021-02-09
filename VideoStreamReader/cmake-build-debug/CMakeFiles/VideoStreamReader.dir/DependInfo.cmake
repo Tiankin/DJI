624f1f143caa82be7964df552d056a6799f5692a
Tiankin <53459017+Tiankin@users.noreply.github.com>
2021-02-09 20:15:54 +0900

    Add files via upload

---
+++ VideoStreamReader/cmake-build-debug/CMakeFiles/VideoStreamReader.dir/DependInfo.cmake
@@ -0,0 +1,41 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/dji/CLionProjects/M210VideoStreamReader/VideoStreamReader/SocketFileServer.cpp" "/home/dji/CLionProjects/M210VideoStreamReader/VideoStreamReader/cmake-build-debug/CMakeFiles/VideoStreamReader.dir/SocketFileServer.cpp.o"
+  "/home/dji/CLionProjects/M210VideoStreamReader/VideoStreamReader/TCPServer.cpp" "/home/dji/CLionProjects/M210VideoStreamReader/VideoStreamReader/cmake-build-debug/CMakeFiles/VideoStreamReader.dir/TCPServer.cpp.o"
+  "/home/dji/CLionProjects/M210VideoStreamReader/VideoStreamReader/common/dji_linux_environment.cpp" "/home/dji/CLionProjects/M210VideoStreamReader/VideoStreamReader/cmake-build-debug/CMakeFiles/VideoStreamReader.dir/common/dji_linux_environment.cpp.o"
+  "/home/dji/CLionProjects/M210VideoStreamReader/VideoStreamReader/common/dji_linux_helpers.cpp" "/home/dji/CLionProjects/M210VideoStreamReader/VideoStreamReader/cmake-build-debug/CMakeFiles/VideoStreamReader.dir/common/dji_linux_helpers.cpp.o"
+  "/home/dji/CLionProjects/M210VideoStreamReader/VideoStreamReader/main.cpp" "/home/dji/CLionProjects/M210VideoStreamReader/VideoStreamReader/cmake-build-debug/CMakeFiles/VideoStreamReader.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "ADVANCED_SENSING"
+  "OPEN_CV_INSTALLED"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/include/libusb-1.0"
+  "../../osdk-core/api/inc"
+  "../../osdk-core/utility/inc"
+  "../../osdk-core/hal/inc"
+  "../../osdk-core/protocol/inc"
+  "../../osdk-core/platform/linux/inc"
+  "../common"
+  "../../osdk-core/modules/inc/payload"
+  "../../osdk-core/modules/inc/flight"
+  "/usr/local/include"
+  "/usr/local/include/opencv"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
