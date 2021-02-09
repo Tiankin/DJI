2a92b1b185e24e5403d7d5f957e65542093a1cbf
Tiankin <53459017+Tiankin@users.noreply.github.com>
2021-02-10 04:33:08 +0900

    Add files via upload

---
+++ M210VideoStreamReader/VideoStreamReader/CMakeFiles/VideoStreamReader.dir/DependInfo.cmake
@@ -0,0 +1,44 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/dji/Desktop/M210VideoStreamReader/VideoStreamReader/SocketFileServer.cpp" "/home/dji/Desktop/M210VideoStreamReader/cmake-build-debug/VideoStreamReader/CMakeFiles/VideoStreamReader.dir/SocketFileServer.cpp.o"
+  "/home/dji/Desktop/M210VideoStreamReader/VideoStreamReader/TCPServer.cpp" "/home/dji/Desktop/M210VideoStreamReader/cmake-build-debug/VideoStreamReader/CMakeFiles/VideoStreamReader.dir/TCPServer.cpp.o"
+  "/home/dji/Desktop/M210VideoStreamReader/VideoStreamReader/common/dji_linux_environment.cpp" "/home/dji/Desktop/M210VideoStreamReader/cmake-build-debug/VideoStreamReader/CMakeFiles/VideoStreamReader.dir/common/dji_linux_environment.cpp.o"
+  "/home/dji/Desktop/M210VideoStreamReader/VideoStreamReader/common/dji_linux_helpers.cpp" "/home/dji/Desktop/M210VideoStreamReader/cmake-build-debug/VideoStreamReader/CMakeFiles/VideoStreamReader.dir/common/dji_linux_helpers.cpp.o"
+  "/home/dji/Desktop/M210VideoStreamReader/VideoStreamReader/main.cpp" "/home/dji/Desktop/M210VideoStreamReader/cmake-build-debug/VideoStreamReader/CMakeFiles/VideoStreamReader.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "ADVANCED_SENSING"
+  "OPEN_CV_INSTALLED"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/include/libusb-1.0"
+  "../VideoStreamReader/../osdk-core/api/inc"
+  "../VideoStreamReader/../osdk-core/utility/inc"
+  "../VideoStreamReader/../osdk-core/hal/inc"
+  "../VideoStreamReader/../osdk-core/protocol/inc"
+  "../VideoStreamReader/../osdk-core/platform/linux/inc"
+  "../VideoStreamReader/common"
+  "../VideoStreamReader/../osdk-core/modules/inc/payload"
+  "../VideoStreamReader/../osdk-core/modules/inc/flight"
+  "/usr/local/include"
+  "/usr/local/include/opencv"
+  "../osdk-core/advanced-sensing-2.0.3/inc"
+  "/usr/include/aarch64-linux-gnu"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/dji/Desktop/M210VideoStreamReader/cmake-build-debug/osdk-core/CMakeFiles/djiosdk-core.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
